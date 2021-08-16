37ae857b815295452c632096299f65a0bf1ba6c2
tonytangdev <tony.tang.dev@gmail.com>
2021-08-16 21:39:51 +0200

    feat: paragraphs

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,6 +8,12 @@ const Card: React.FunctionComponent<ICardProps> = (props) => {
     return (
         <>
             <Hero />
+            <h2>
+                Order Summary
+            </h2>
+            <p>
+                You can now listen to milions of songs, audiobooks, and podcasts on any device anywhere you like !
+            </p>
         </>
     )
 }
